c1a5088fe2367799f5f4361fdb4f3d156a4a1297
Jan Szejko <janek37@gmail.com>
2017-06-13 15:38:27 +0200

    No http sources - fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def whole_trees(package_dir, paths):
 
 setup(
     name='django-sponsors',
-    version='1.2',
+    version='1.2.1',
     author='Marek Stępniowski',
     author_email='marek@stepniowski.com',
     maintainer='Jan Szejko',
